139b8765898faa3595bce5a4e6fd7419ad8c1b4e
skorikartem <94537430+skorikartem@users.noreply.github.com>
2022-02-16 19:51:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let table = document.getElementById('age-table')
+
+table.getElementsByTagName('label')
+
+table.querySelector('td')
+
+let form = document.getElementsByName('search')[0]
+
+form.querySelector('input')
+
+let inputs = form.querySelectorAll('input')
+inputs[inputs.length-1]
